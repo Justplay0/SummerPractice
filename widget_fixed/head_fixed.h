166ff05cf00fb21f1c49dc843213ea40343804f9
mingye0721 <134084659+mingye0721@users.noreply.github.com>
2023-06-27 17:30:00 +0800

    Add files via upload

---
+++ widget_fixed/head_fixed.h
@@ -0,0 +1,17 @@
+#ifndef HEAD_FIXED_H
+#define HEAD_FIXED_H
+#include<QObject>
+#include <QDebug>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include<QMessageBox>
+#include<QDateTime>
+#include<QString>
+#include<QLabel>
+#include<QHBoxLayout>
+#include<QVBoxLayout>
+#include <QToolButton>
+#include<QCheckBox>
+#include <QTimer>
+#endif // HEAD_FIXED_H
